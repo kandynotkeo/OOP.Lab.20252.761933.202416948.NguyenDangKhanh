b0d5e3e86673a514fb8fd7a7be07c5c6e2fbb7cb
kandy.not.keo <kandy.forum.975@gmail.com>
2026-03-26 15:12:29 +0700

    src: 2.2.2, 2.2.3, 2.2.4
    2.2.2 update FirstDialog
    2.2.3 HelloNameDialog
    2.2.4 ShowTwoNumbers

--- Lab01/FirstDialog.java
+++ Lab01/FirstDialog.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class FirstDialog {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello World! How are you?");
+        JOptionPane.showMessageDialog(null, "Hello World!");
         System.exit(0);
     }
 }
