1d6c1c9fc7ee1dfe322d8a5093eda246cb7c2aa7
kandy.not.keo <kandy.forum.975@gmail.com>
2026-03-27 13:28:54 +0700

    src: 2.2.5
    2.2.5 Two Doubles

---
+++ Lab01/TwoDoubles.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TwoDoubles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Two numbers: ");
+        double num1 = input.nextDouble(), num2 = input.nextDouble();
+        System.out.println("Sum: " + (num1 + num2));
+        System.out.println("Difference: " + (num1 - num2));
+        System.out.println("Product: " + num1 * num2);
+        System.out.println("Quotient: " + num1 / num2);
+        System.exit(0);
+    }
+}
