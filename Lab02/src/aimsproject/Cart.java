e4d35da6a3e216b9337e725084ce79c86c96a7bc
kandynotkeo <kandy.forum.975@gmail.com>
2026-04-03 23:51:24 +0700

    src: Lab02 aimsproject
    AIMS class
    Cart class
    DigitalVideoDisc class

---
+++ Lab02/src/aimsproject/Cart.java
@@ -0,0 +1,37 @@
+package aimsproject;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private int qtyOrdered = 0;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+
+    public void addDigitalVideoDisk(DigitalVideoDisc disc) {
+        if (qtyOrdered >= MAX_NUMBERS_ORDERED) {
+            System.out.println("The cart is full.");
+            return;
+        }
+        itemsOrdered[qtyOrdered] = disc;
+        qtyOrdered++;
+        System.out.println("Added to cart.");
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                for (int j = i; j < qtyOrdered - 1; j++) itemsOrdered[j] = itemsOrdered[j + 1];
+                qtyOrdered--;
+                itemsOrdered[qtyOrdered] = null;
+                System.out.println("Removed from cart.");
+                return;
+            }
+        }
+        System.out.println("Not exist in cart.");
+    }
+
+    public float totalCost() {
+        float cost = 0f;
+        for (int i = 0; i < qtyOrdered; i++)
+            cost += itemsOrdered[i].getCost();
+        return cost;
+    }
+}
