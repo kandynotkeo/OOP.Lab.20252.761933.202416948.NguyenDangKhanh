4d743bb40a0f3978feac57e201d828cd52a6803b
kandynotkeo <khanhnd.975@gmail.com>
2026-04-20 01:15:19 +0700

    topic: method overloading
    add method overloading for addDigitalVideoDisc
    - original: 1 DVD object
    - varargs: arbitrary number of DVD object, or DVD array
    - 2args: 2 DVD objects

---
+++ Lab03/AimsProject/src/AIMS.java
@@ -0,0 +1,22 @@
+public class AIMS {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+
+        anOrder.addDigitalVideoDisc(new DigitalVideoDisc[]{dvd1, dvd2});
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        System.out.println("The total cost: " + anOrder.totalCost());
+
+        anOrder.removeDigitalVideoDisc(dvd3);
+        System.out.println("The total cost: " + anOrder.totalCost());
+
+        anOrder.removeDigitalVideoDisc(dvd1);
+        System.out.println("The total cost: " + anOrder.totalCost());
+
+        anOrder.removeDigitalVideoDisc(dvd2);
+        System.out.println("The total cost: " + anOrder.totalCost());
+    }
+}
