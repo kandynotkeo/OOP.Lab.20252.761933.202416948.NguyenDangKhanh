18bca6a57961acf052fae0a04d8d106a45374c5a
kandynotkeo <khanhnd.975@gmail.com>
2026-04-21 14:55:32 +0700

    refactor: packages
    Organize source base using packages

---
+++ Lab03/AimsProject/src/hust/soict/hedspi/aims/disc/DigitalVideoDisc.java
@@ -0,0 +1,104 @@
+package hust.soict.hedspi.aims.disc;
+
+public class DigitalVideoDisc {
+    // classifier properties
+    private static int nbDigitalVideoDiscs = 0;
+
+    // instance properties
+    private final int id;
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    // constructors
+    public DigitalVideoDisc() {
+        nbDigitalVideoDiscs++;
+        id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title) {
+        this();
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this(title);
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this(title, category, cost);
+        this.director = director;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this(title, category, director, cost);
+        this.length = length;
+    }
+
+    // classifier methods
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
+
+    // getters, setters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        if (title == null || title.isEmpty()) return "Unknown";
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        if (title == null) return "Unclassified";
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDirector() {
+        if (director == null) return "Anonymous";
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    // instance methods
+    @Override
+    public String toString() {
+        return "DVD - " + getTitle() + " - " + getCategory() + " - " + getDirector() + " - " + (length < 0 ? "N/A" : length + " mins") + ": " + (cost < 0 ? "N/A" : cost + "$");
+    }
+
+    public boolean isMatch(String title) {
+        return this.title.equals(title);
+    }
+}
---
+++ Lab03/AimsProject/src/hust/soict/hedspi/test/store/StoreTest.java
@@ -0,0 +1,21 @@
+package hust.soict.hedspi.test.store;
+
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+import hust.soict.hedspi.aims.store.Store;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        Store store = new Store();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+
+        store.addDVD(dvd1, dvd2, dvd3);
+        store.print();
+        store.removeDVD(dvd1);
+        store.print();
+        store.removeDVD(dvd2);
+        store.print();
+    }
+}
